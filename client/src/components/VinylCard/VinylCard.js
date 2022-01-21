c528d7e9e4e55569935f1e8de371b88995414c3d
rytc <rytc@protonmail.com>
2022-01-21 09:25:37 -0800

    Added hover info to VinylCard

--- client/src/components/VinylCard/VinylCard.js
+++ client/src/components/VinylCard/VinylCard.js
@@ -4,7 +4,13 @@ const VinylCard = (props) => {
 
     return (
         <div className="Vinyl-card" onClick={props.onClick}>
+            
+
             <img src={data.albumart} alt="Album art" />
+            <div className="Vinyl-info">
+                <h4>{data.title}</h4>
+                <h5>{data.artists[0]}</h5>
+            </div>
         </div>
     );
 }
